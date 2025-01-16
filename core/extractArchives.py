57adae2a9fd4a2614c63a32c6b94fcf5d8851635
crowdedmovie <crowdedmovie14@proton.me>
2025-01-16 19:22:30 +0100

    Add program files and directory structure

---
+++ core/extractArchives.py
@@ -0,0 +1,126 @@
+import os
+import subprocess
+import logging
+from time import perf_counter
+from PyQt6.QtCore import QThread, pyqtSignal
+
+logging.basicConfig(
+    filename="logs.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+class ArchiveExtractor(QThread):
+    finished = pyqtSignal(float)
+    progress_updated = pyqtSignal(int, int, float, float)
+
+    def __init__(self, ui, source_folder, destination_folder, selected_formats):
+        super().__init__()
+        self.ui = ui
+        self.source_folder = source_folder
+        self.destination_folder = destination_folder
+        self.selected_formats = selected_formats
+        self.total_files = 0
+        self.processed_files = 0
+        self.total_size = 0
+        self.processed_size = 0
+
+    def run(self):
+        if not os.path.isdir(self.source_folder):
+            self.update_log(f"The source folder '{self.source_folder}' does not exist.", "error")
+            return
+
+        self.calculate_totals()
+        start_time = perf_counter()
+
+        for root, dirs, files in os.walk(self.source_folder):
+            if self.isInterruptionRequested():
+                break
+            relative_path = os.path.relpath(root, self.source_folder)
+            destination_subfolder = os.path.join(self.destination_folder, relative_path)
+
+            if not os.path.exists(destination_subfolder):
+                os.makedirs(destination_subfolder)
+
+            for archive in files:
+                if self.isInterruptionRequested():
+                    break
+                archive_path = os.path.join(root, archive)
+                if self.is_supported_archive(archive_path):
+                    self.extract_archive(archive_path, destination_subfolder)
+
+        end_time = perf_counter()
+        total_time = round(end_time - start_time, 2)
+        # noinspection PyUnresolvedReferences
+        self.finished.emit(total_time)
+
+    def calculate_totals(self):
+        for root, _, files in os.walk(self.source_folder):
+            for file in files:
+                file_path = os.path.join(root, file)
+                if self.is_supported_archive(file_path):
+                    self.total_files += 1
+                    self.total_size += os.path.getsize(file_path)
+
+        # bytes to gigabytes
+        print(self.total_size)
+        # noinspection PyUnresolvedReferences
+        self.progress_updated.emit(self.processed_files, self.total_files, self.processed_size, self.total_size)
+
+    def is_supported_archive(self, archive_path):
+        return any(archive_path.endswith(extension) for extension in self.selected_formats)
+
+    def extract_archive(self, archive_path, destination_folder):
+        if not os.path.isfile(archive_path):
+            self.update_log(f"The archive '{archive_path}' does not exist.", "error")
+            return
+
+        start_time = perf_counter()
+
+        try:
+            if archive_path.endswith(".rar"):
+                command = ["unrar", "x", "-y", archive_path, destination_folder]
+            else:
+                command = ["7z", "x", "-y", archive_path, f"-o{destination_folder}"]
+
+            result = subprocess.run(command, check=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+            if result.returncode == 0:
+                end_time = perf_counter()
+                duration = round(end_time - start_time, 2)
+                self.update_log(
+                    f"Extraction successful: {archive_path} -> {destination_folder} (in {duration}s)", "success"
+                )
+                self.processed_files += 1
+                self.processed_size += os.path.getsize(archive_path)
+                # noinspection PyUnresolvedReferences
+                self.progress_updated.emit(
+                    self.processed_files, self.total_files, self.processed_size, self.total_size
+                )
+
+            else:
+                self.update_log(f"Extraction failed for {archive_path}. Command output: {result.stderr.strip()}", "error")
+
+        except subprocess.CalledProcessError as e:
+            self.update_log(f"Error: {str(e)} during extraction of {archive_path}", "error")
+
+    def update_log(self, message, status):
+        if status == "success":
+            logging.info(message)
+            text_color = "green"
+        else:
+            logging.error(message)
+            text_color = "red"
+
+        gv_message = f'<font color="{text_color}">{message}</font>'
+        self.ui.gvDecompressionLogs.append(gv_message)
+        self.ui.gvDecompressionLogs.verticalScrollBar().setValue(
+            self.ui.gvDecompressionLogs.verticalScrollBar().maximum()
+        )
+
+        # TODO: Modify log file formats for decompression events:
+        #  Decompression started: decompression of [source_folder] to [destination_folder] started, [y] files will be processed
+        #  Decompression finished: decompression of [source_folder] to [destination_folder] finished, [x/y] files processed
+        #  Decompression canceled: decompression of [source_folder] to [destination_folder] canceled, [x/y] files processed
+
+
---
+++ gui/mainWindow.py
@@ -0,0 +1,235 @@
+from PyQt6.QtWidgets import QMainWindow, QFileDialog, QMessageBox, QApplication
+from gui.gui_interface import Ui_Main
+from core.extractArchives import ArchiveExtractor
+import os
+import platform
+import subprocess
+import shutil
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.extractor = None
+        self.ui = Ui_Main()
+        self.ui.setupUi(self)
+
+        self.check_dependencies()
+        # TODO : add img to Main Window, eventually error / info window
+
+        self.ui.btnSelectSource.clicked.connect(self.browse_source_folder)
+        self.ui.btnSelectDestination.clicked.connect(self.browse_destination_folder)
+        self.ui.btnStartDecompression.clicked.connect(self.start_extraction)
+        self.ui.btnClearLogs.clicked.connect(self.clear_logs)
+        self.ui.actionOpenLogs.triggered.connect(self.open_logs_file)
+        self.ui.actionExit.triggered.connect(self.close_app)
+        self.ui.btnCancelDecompression.clicked.connect(self.cancel_extraction)
+
+    def browse_source_folder(self):
+        folder = QFileDialog.getExistingDirectory(self, "Select source folder")
+        if folder:
+            if os.path.isdir(folder):
+                self.ui.txtSourceFolder.setText(folder)
+            else:
+                QMessageBox.critical(self, "Error", "Source folder does not exist.")
+        else:
+            pass
+
+    def browse_destination_folder(self):
+        folder = QFileDialog.getExistingDirectory(self, "Select destination folder")
+        if folder:
+            if os.path.isdir(folder):
+                self.ui.txtDestinationFolder.setText(folder)
+            else:
+                QMessageBox.critical(self, "Error", "Destination folder does not exist.")
+        else:
+            pass
+
+    def get_selected_formats(self):
+        format_checkboxes = {
+            ".zip": self.ui.chkZipFormat,
+            ".rar": self.ui.chkRarFormat,
+            ".tar.gz": self.ui.chkTarGzFormat,
+            ".tar.bz2": self.ui.chkTarBz2Format,
+            ".7z": self.ui.chk7zFormat,
+            ".tar": self.ui.chkTarFormat
+        }
+
+        selected_formats = [format_ for format_, checkbox in format_checkboxes.items() if checkbox.isChecked()]
+        return selected_formats
+
+    def update_ui_state(self, state):
+        for checkbox in [
+            self.ui.chkRarFormat,
+            self.ui.chkZipFormat,
+            self.ui.chkTarGzFormat,
+            self.ui.chkTarBz2Format,
+            self.ui.chk7zFormat,
+            self.ui.chkTarFormat
+        ]:
+            checkbox.setEnabled(state)
+
+        for btn in [
+            self.ui.btnSelectDestination,
+            self.ui.btnSelectSource,
+            self.ui.btnStartDecompression
+        ]:
+            btn.setEnabled(state)
+
+        for txtLine in [
+            self.ui.txtSourceFolder,
+            self.ui.txtDestinationFolder
+        ]:
+            txtLine.setEnabled(state)
+
+    def start_extraction(self):
+        source_folder = self.ui.txtSourceFolder.text()
+        destination_folder = self.ui.txtDestinationFolder.text()
+        selected_formats = self.get_selected_formats()
+
+        if not source_folder:
+            QMessageBox.warning(self, "Warning", "Please select a source folder!")
+            return
+
+        if not destination_folder:
+            QMessageBox.warning(self, "Warning", "Please select a destination folder!")
+            return
+
+        if not selected_formats:
+            QMessageBox.warning(self, "Warning", "Please select at least one archive format!")
+            return
+
+        found_archive = False
+        for root, dirs, files in os.walk(source_folder):
+            if any(file.endswith(ext) for ext in selected_formats for file in files):
+                found_archive = True
+
+        if not found_archive:
+            QMessageBox.warning(self, "Warning", "No archives of the selected format(s) found in the source folder!")
+            return
+
+        self.clear_processed_lbl()
+        self.update_ui_state(False)
+
+        self.ui.progressBarDecompression.setMaximum(100)
+        self.ui.progressBarDecompression.setValue(0)
+        self.ui.progressBarDecompression.setStyleSheet("")
+
+        self.extractor = ArchiveExtractor(self.ui, source_folder, destination_folder, selected_formats)
+        self.extractor.progress_updated.connect(self.update_progress_labels)
+        self.extractor.finished.connect(self.on_extraction_finished)
+        self.extractor.start()
+
+    def update_progress_labels(self, processed_files, total_files, processed_size, total_size):
+
+        def format_size(size_in_bytes):
+            # format the size in KB, MB, or GB
+            if size_in_bytes < 1024:
+                return f"{size_in_bytes} B"
+            elif size_in_bytes < 1024 ** 2:
+                size_in_kb = size_in_bytes / 1024
+                return f"{size_in_kb:.0f} KB"
+            elif size_in_bytes < 1024 ** 3:
+                size_in_mb = size_in_bytes / 1024 ** 2
+                return f"{size_in_mb:.0f} MB"
+            else:
+                size_in_gb = size_in_bytes / 1024 ** 3
+                return f"{size_in_gb:.2f} GB"
+
+        file_progress = (processed_files / total_files) * 100
+        size_progress = (processed_size / total_size) * 100
+        overall_progress = ((file_progress + size_progress) / 2)
+
+        processed_size_formatted = format_size(processed_size)
+        total_size_formatted = format_size(total_size)
+
+        self.ui.lblFilesProcessed.setText(f"Files processed: {processed_files} / {total_files}")
+        self.ui.lblDataProcessed.setText(f"Data processed: {processed_size_formatted} / {total_size_formatted}")
+        self.ui.progressBarDecompression.setValue(int(overall_progress))
+        self.ui.lblProgressBarValue.setText(f"{self.ui.progressBarDecompression.value()}%")
+
+    def on_extraction_finished(self, total_time):
+        self.update_ui_state(True)
+        QMessageBox.information(
+            self,
+            "Extraction Complete",
+            f"Extraction finished successfully in {total_time:.2f} seconds!"
+        )
+
+    def on_extraction_cancelled(self):
+        self.update_ui_state(True)
+        QMessageBox.information(self, "Extraction Cancelled", "The extraction process has sucessfully been cancelled.")
+        self.ui.gvDecompressionLogs.append('<font color="red">Extraction cancelled by the user.</font>')
+        self.ui.progressBarDecompression.setStyleSheet("QProgressBar::chunk{background-color: #06b025;}")
+
+    def clear_logs(self):
+        self.ui.gvDecompressionLogs.clear()
+        self.clear_processed_lbl()
+
+    def clear_processed_lbl(self):
+        self.ui.lblFilesProcessed.setText("Files processed: 0 / 0")
+        self.ui.lblDataProcessed.setText("Data processed: 0 / 0 B")
+        self.ui.progressBarDecompression.setValue(0)
+        self.ui.lblProgressBarValue.setText("0%")
+
+    def open_logs_file(self):
+        logs_file = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "logs.log"))
+        system = platform.system()
+
+        try:
+            if system == "Windows":
+                os.startfile(logs_file)
+            elif system == "Linux":
+                subprocess.run(["xdg-open", logs_file])
+            elif system == "Darwin":
+                subprocess.run(["open", logs_file])
+            else:
+                QMessageBox.warning(self, "Warning", "System not supported for opening log file.")
+
+        except Exception as e:
+            QMessageBox.warning(self, "Warning", f"Error opening log file : {e}")
+
+    def close_app(self):
+        QApplication.quit()
+
+    def cancel_extraction(self):
+        if self.extractor and self.extractor.isRunning():
+            self.extractor.terminate()
+            self.extractor.wait()
+            self.on_extraction_cancelled()
+        else:
+            pass  # no message box for when cancelling none existing process
+
+    def check_dependencies(self):
+        required_dependencies = {
+            "7z": "7-Zip",
+            "unrar": "UnRAR"
+        }
+
+        missing_dependencies = []
+        for cmd_command, name in required_dependencies.items():
+            if not shutil.which(cmd_command):
+                missing_dependencies.append(name)
+
+        unsupported_formats = []
+        dependencies_to_formats = {
+            "7-Zip": ["7z", "Zip", "TarGz", "TarBz2"],
+            "UnRAR": ["Rar"]
+        }
+
+        for dependency, formats in dependencies_to_formats.items():
+            if dependency in missing_dependencies:
+                unsupported_formats.extend(formats)
+
+        unsupported_formats_str = ", ".join(unsupported_formats)
+
+        if missing_dependencies:
+            missing_str = ", ".join(missing_dependencies)
+            QMessageBox.critical(self, "Missing Dependencies", (
+                f"The following dependencies are missing or not accessible from the PATH: {missing_str}\n\n"
+                f"As a result, the following formats will not be supported: {unsupported_formats_str}\n\n"
+                "To resolve this issue, please install the required dependencies or add them to the system PATH."
+            ))
+            self.close()
+
+
